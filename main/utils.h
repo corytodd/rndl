633a5e44b93719715162e93a92a872c589285491
Cory Todd <cory@corytodd.us>
2024-02-03 22:32:32 -0800

    utils: add standard util macros

--- main/utils.h
+++ main/utils.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include <stdint.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define UNUSED(x)          (void)(x)
 #define MAX(a, b)          ((a) > (b) ? (a) : (b))
 #define MIN(a, b)          ((a) < (b) ? (a) : (b))
 #define CLAMP(x, min, max) (MAX((min), MIN((max), (x))))
 #define PACKED             __attribute__((packed))
-
-typedef void (*fn_delay_ms)(uint32_t ms);
+#define KHZ(x)             ((x) * 1000)
+#define MHZ(x)             ((x) * 1000000)
+#define KB(x)              ((x) * 1024)
+#define MB(x)              ((x) * 1024 * 1024)
+#define FPS_TO_MS(fps)     (1000 / (fps))
 
 #ifdef __cplusplus
 }
