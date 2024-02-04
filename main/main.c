93f758e7e8d5769c978177a5e3d8dd8ca15d1e48
Cory Todd <cory@corytodd.us>
2024-02-04 11:44:07 -0800

    main: add strobing rectangle pattern
    A rectangle that shrinks to a point then starts over.

--- main/main.c
+++ main/main.c
@@ -27,29 +27,60 @@ const surface_config_t surface_config = {
     .height = CONFIG_LED_ROWS,
 };
 
-void app_main(void) {
+// Loop a shrinking rectangle forever
+static void pattern_strobing_rectangle(void) {
+    // TODO: the driver should control what "off" means, not the renderer
     const color_t color_off = {0};
     hsv_t hsv = {.hue = 100, .saturation = 100, .value = 50};
     color_t color = {0};
-    point_t point = {0};
-
-    ESP_ERROR_CHECK(led_panel_driver_new(&config, &led_driver));
-    ESP_ERROR_CHECK(surface_create(&surface_config, led_driver, &surface));
-
-    ESP_LOGI(TAG, "startup complete");
 
     rect_t rect = {
         .top_left = {0, 0},
         .bottom_right = {CONFIG_LED_COLS - 1, CONFIG_LED_ROWS - 1},
     };
+
+    int counter = 0;
     while (1) {
         color_hsv2rgb(&hsv, &color);
 
         surface->clear(surface, &color_off);
         surface->draw_rect(surface, &rect, NULL, &color);
         surface->render(surface);
-        point.y = (point.y + 1) % CONFIG_LED_ROWS;
 
-        ++hsv.hue;
+        ++counter;
+
+        rect.top_left.x = (rect.top_left.x + 1) % (CONFIG_LED_COLS / 2);
+        rect.top_left.y = (rect.top_left.y + 1) % (CONFIG_LED_ROWS / 2);
+
+        rect.bottom_right.x = (rect.bottom_right.x - 1);
+        rect.bottom_right.y = (rect.bottom_right.y - 1);
+
+        if (rect.bottom_right.x < (CONFIG_LED_COLS / 2)) {
+            rect.bottom_right.x = CONFIG_LED_COLS - 1;
+        }
+        if (rect.bottom_right.y < (CONFIG_LED_ROWS / 2)) {
+            rect.bottom_right.y = CONFIG_LED_ROWS - 1;
+        }
+        if (rect.bottom_right.y - rect.top_left.y <= 1 || rect.bottom_right.x - rect.top_left.x <= 1) {
+            rect.top_left.x = 0;
+            rect.top_left.y = 0;
+            rect.bottom_right.x = CONFIG_LED_COLS - 1;
+            rect.bottom_right.y = CONFIG_LED_ROWS - 1;
+        }
+        hsv.hue++;
+    }
+}
+
+void app_main(void) {
+    ESP_LOGI(TAG, "initializing");
+    ESP_ERROR_CHECK(led_panel_driver_new(&config, &led_driver));
+    ESP_ERROR_CHECK(surface_create(&surface_config, led_driver, &surface));
+    ESP_LOGI(TAG, "startup complete");
+
+    pattern_strobing_rectangle();
+
+    ESP_LOGI(TAG, "pattern complete");
+
+    for (;;) {
     }
 }
