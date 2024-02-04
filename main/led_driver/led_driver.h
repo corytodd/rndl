159cd816b54e984610aad4ef8283f9f32b9a8c5f
Cory Todd <cory@corytodd.us>
2024-02-03 22:39:08 -0800

    led_driver: add config type
    Add a base type for the led driver config. This is intended to be a
    member of implementation specific driver config structs.

--- main/led_driver/led_driver.h
+++ main/led_driver/led_driver.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "platform.h"
+
 #include <esp_err.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -28,6 +30,15 @@ struct led_driver_t {
     esp_err_t (*led_driver_write_blocking)(led_driver_t *driver, const void *data, size_t size__bytes,
                                            uint32_t timeout__ms);
 };
+
+typedef struct led_driver_config {
+    uint16_t gpio_num;      /*!< GPIO number */
+    uint32_t led_count;     /*!< Number of LEDs */
+    uint32_t resolution_hz; /*!< Resolution in Hz */
+    uint32_t frame_time_ms; /*!< Time in milliseconds to hold each frame */
+    fn_delay_ms fn_delay;   /*!< Delay function */
+    int timeout_ms;         /*!< Timeout in milliseconds for blocking operations*/
+} led_driver_config_t;
 
 #ifdef __cplusplus
 }
