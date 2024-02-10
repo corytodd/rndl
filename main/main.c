1d4d84c284a3cb2d49ee97bea9c3a856c382c85d
Cory Todd <cory@corytodd.us>
2024-02-10 14:40:44 -0800

    example: remove rendering task
    The examples are not designed to generate frames so we're effectively
    double rendering. Maybe... we could instead double-buffer to make this
    actually useful.

--- main/main.c
+++ main/main.c
@@ -4,7 +4,6 @@
 #include <esp_log.h>
 #include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
-#include <freertos/event_groups.h>
 #include <freertos/task.h>
 #include <rndl/graphics/surface.h>
 #include <rndl/led_driver/led_driver.h>
@@ -31,59 +30,15 @@ static const rndl_surface_config_t surface_config = {
     .height = CONFIG_RNDL_ROWS,
 };
 
-static EventGroupHandle_t core_events;
-typedef enum {
-    core_event_none = 0,
-    core_event_render_timer = BIT0, /*!< A frame is ready to render */
-} core_events_t;
-
-static void render_timer_cb(void *arg) {
-    BaseType_t xHigherPriorityTaskWoken, xResult;
-    xHigherPriorityTaskWoken = pdFALSE;
-
-    xResult = xEventGroupSetBitsFromISR(core_events, core_event_render_timer, &xHigherPriorityTaskWoken);
-
-    if (xResult != pdFAIL) {
-        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-    }
-}
-
-static void render_task(void *pvParameters) {
-
-    const esp_timer_create_args_t render_timer_args = {
-        .callback = &render_timer_cb,
-        .name = "render_timer",
-    };
-    esp_timer_handle_t render_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&render_timer_args, &render_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(render_timer, MS_TO_US(RNDL_FPS_TO_MS(CONFIG_RNDL_FPS))));
-
-    ESP_LOGI(TAG, "render task started");
-
-    for (;;) {
-        EventBits_t bits = xEventGroupWaitBits(core_events, core_event_render_timer, pdTRUE, pdFALSE, portMAX_DELAY);
-        if (bits & core_event_render_timer) {
-            surface->render(surface);
-        }
-    }
-}
-
 void app_main(void) {
     ESP_LOGI(TAG, "initializing");
 
     ESP_ERROR_CHECK(rndl_led_panel_driver_new(&config, &led_driver));
     ESP_ERROR_CHECK(rndl_surface_create(&surface_config, led_driver, &surface));
 
-    core_events = xEventGroupCreate();
-    ESP_ERROR_CHECK(core_events == NULL ? ESP_FAIL : ESP_OK);
-
-    BaseType_t xResult = xTaskCreate(render_task, "render_task", CONFIG_RNDL_RENDER_STACK_SIZE_BYTES, NULL,
-                                     CONFIG_RNDL_RENDER_PRIORITY, NULL);
-    ESP_ERROR_CHECK(xResult != pdPASS ? ESP_FAIL : ESP_OK);
-
     ESP_LOGI(TAG, "startup complete");
 
-    example_bitmap(surface, &surface_config);
+    example_raindrops(surface, &surface_config);
 
     ESP_LOGI(TAG, "pattern complete");
 
