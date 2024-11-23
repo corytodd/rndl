2a0cc9b4f4456a1b73afabbc915d9a1138a2d549
Cory Todd <cory@corytodd.us>
2024-11-23 09:35:24 -0800

    add c++ class for init.py

---
+++ components/rndl/Rndl.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace rndl {
+
+class Rtttl : public Component {};
+
+} // namespace rndl
+} // namespace esphome
